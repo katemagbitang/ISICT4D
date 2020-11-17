144e6175921c8dc5d86d871183a08aaf966ddd93
Magbitang <julianne_kristine_magbitang@dlsu.edu.ph>
2020-11-18 01:41:47 +0800

    routes folder

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-// const hbs = require('hbs');
+const hbs = require('hbs');
+const bodyparser = require('body-parser');
 const PORT = 3000;
 
-app.get('/', function(req,res){
-    res.send('Hello world!');
-});
+const routes = require('./routes/routes.js');
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + "/public"));
+
+// parses incoming requests with urlencoded payloads
+app.use(express.urlencoded({extended: true}));
+
+app.use('/',routes);
 
 app.listen(PORT, function(){
     console.log('App listening at port ' + PORT)
