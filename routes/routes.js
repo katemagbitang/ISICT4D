c71b13c4120fe5f25daae7b7a1ceba6d44e77e02
Magbitang <julianne_kristine_magbitang@dlsu.edu.ph>
2020-11-18 19:03:11 +0800

    added controller and bootstrap

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
+const controller = require('../controller/controller.js');
 
-app.get('/', function(req,res){
-    res.render('index',{});
-});
+app.get('/', controller.getIndex);
 
 module.exports = app;
